4c4b11475dea5d2c635404977196111b57975b3b
Lecogoni <giraud.nicolas@me.com>
2021-03-17 22:18:42 +0100

    modiuf de application_controler pour prise en compte de username lors de sign_up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+
+  before_action :configure_devise_parameters, if: :devise_controller?
+
+
+  # Method qui permet a devise de prendre :username en compte lors du sign_up
+  def configure_devise_parameters
+    devise_parameter_sanitizer.permit(:sign_up) {|u| u.permit(:username, :email, :password, :password_confirmation)}
+  end
+
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :events
+  has_many :events, dependent: :destroy
   has_many :challenges, through: :events
 
   #after_create :welcome_send
