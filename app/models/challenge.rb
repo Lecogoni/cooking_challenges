aa01a45ae6416673f42c94be1ffbb2567bb1aa28
Lecogoni <giraud.nicolas@me.com>
2021-03-17 21:17:10 +0100

    ajout de controle sur nombre de particpants admis (integer >= 1) pour Challenge > invite_number

--- app/models/challenge.rb
+++ app/models/challenge.rb
@@ -4,5 +4,14 @@ class Challenge < ApplicationRecord
   has_many :users, through: :events
 
   has_many :invites, dependent: :destroy
+
+  validates :invite_number, {
+    :presence => true,
+    :allow_blank => false,
+    :allow_nil => false,
+  }
+
+  validates :invite_number, numericality: { only_integer: true }
+  validates :invite_number, :numericality => {:greater_than_or_equal_to => 1}
   
 end
