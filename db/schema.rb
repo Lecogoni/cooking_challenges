4cdf6282910769bd8695d40084c1c5d7a5a14cf4
Lecogoni <giraud.nicolas@me.com>
2021-03-19 11:47:29 +0100

    rename event status migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_19_102407) do
+ActiveRecord::Schema.define(version: 2021_03_19_103950) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,7 +25,7 @@ ActiveRecord::Schema.define(version: 2021_03_19_102407) do
   end
 
   create_table "events", force: :cascade do |t|
-    t.string "event_statut", default: "unscheduled"
+    t.string "status", default: "unscheduled"
     t.string "role", default: "participant"
     t.bigint "user_id"
     t.bigint "challenge_id"
