76844477889e3af59be323a855a8411daaf451ea
Lecogoni <giraud.nicolas@me.com>
2021-03-21 11:24:58 +0100

    drop useless table table_survey

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_20_170207) do
+ActiveRecord::Schema.define(version: 2021_03_21_102313) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -63,15 +63,6 @@ ActiveRecord::Schema.define(version: 2021_03_20_170207) do
     t.index ["surveyor_id"], name: "index_surveys_on_surveyor_id"
   end
 
-  create_table "table_surveys", force: :cascade do |t|
-    t.bigint "event_id"
-    t.bigint "surveyor_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["event_id"], name: "index_table_surveys_on_event_id"
-    t.index ["surveyor_id"], name: "index_table_surveys_on_surveyor_id"
-  end
-
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
@@ -90,5 +81,4 @@ ActiveRecord::Schema.define(version: 2021_03_20_170207) do
   add_foreign_key "guests", "challenges"
   add_foreign_key "questions", "surveys"
   add_foreign_key "surveys", "events"
-  add_foreign_key "table_surveys", "events"
 end
